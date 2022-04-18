233583bd51e0bbbbb04cc442037f709453ef0b2b
Joao7747 <joaoguilherme.sbc@gmail.com>
2022-04-18 20:50:06 -0300

    inicial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MapaComponent } from './views/home/mapa/mapa.component';
+import {GMapModule} from 'primeng/gmap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapaComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    GMapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/home/mapa/mapa.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.css']
+})
+
+export class MapaComponent {
+
+  options : any;
+
+  overlays: any[] | undefined;
+
+    ngOnInit() {
+        this.options = {
+            center: {lat: -23.533773, lng: -46.625290},
+            zoom: 12
+        };
+    }
+}
