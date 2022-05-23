9abdc3ea90fe1c11d3f5a27701962654c4f5672f
CaioMorais <caiomorais99@outlook.com>
2022-05-22 22:26:15 -0300

    login button id

--- src/app/shared/models/criar-conta-request.ts
+++ src/app/shared/models/criar-conta-request.ts
@@ -1,9 +1,9 @@
 export class Conta{
     nome!: string;
-    cpf!: string;  
-    email!: string; 
+    cpf!: string;
+    email!: string;
     senha!: string; 
-    tipoConta!: string;  
+    tipoConta!: string;
     nomeEstabelecimento!: string;
     tipoEstabelecimento!: string;
     cnpj!: string;
